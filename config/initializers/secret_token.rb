42ce86a46d369bec7661a7fa3ceca4e84f708b97
murata <eringimazui@gmail.om>
2010-10-04 17:20:27 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZantareMurata::Application.config.secret_token = 'f1831f1f667f63fe523a2dd2e16bb4816de545be076009001fcb75c3af152cc9d86846e3416b65c8d9e8e07805830f0cded91ef154eb27f123fcba013184f582'
